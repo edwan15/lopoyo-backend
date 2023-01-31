c3807934093ee27dc6136b70b53ca20c8b350d29
Edwan zapata <99996593+edwan15@users.noreply.github.com>
2023-01-31 18:06:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://inventario-agrod.onrender.com/"],
     credentials: true,
   })
 );
